ea02842793f1c7a172ff1629a79ff9ef07a59706
Kumar Abhishek <61007819+abhishek8434@users.noreply.github.com>
2024-12-28 18:15:37 +0530

    Update seleniumwithpytest.py

--- tests/seleniumwithpytest.py
+++ tests/seleniumwithpytest.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
